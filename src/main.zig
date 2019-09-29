0d97de190e4007d7a0e38e6c1960480d4386eeda
Sebastian Keller <sebastiankeller@fastmail.net>
2019-09-29 04:23:01 +0200

    Fixed typo

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const testing = std.testing;
 
 pub const Mat2 = @import("mat2.zig").Mat2;
-pub const Vec2 = @import("vec2.zig").Mat2;
+pub const Vec2 = @import("vec2.zig").Vec2;
 
 test "zig_matrix" {
     _ = @import("mat2.zig");
