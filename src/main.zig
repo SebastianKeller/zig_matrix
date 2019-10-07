9d4d3d4a26e9801bc9d2fbbf18d31ef821d73b98
Sebastian Keller <sebastiankeller@fastmail.net>
2019-10-07 22:33:24 +0200

    Fixed main.zig

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ pub const Mat4 = @import("mat4.zig").Mat4;
 pub const Vec2 = @import("vec2.zig").Vec2;
 pub const Vec3 = @import("vec3.zig").Vec3;
 pub const Vec4 = @import("vec4.zig").Vec4;
-pub const Quat = @import("quat.zig").Vec4;
+pub const Quat = @import("quat.zig").Quat;
 
 test "zig_matrix" {
     _ = @import("mat2.zig").Mat2;
